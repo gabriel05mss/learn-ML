257c2e3448c75af5e2e8ba371a921bce04888a0f
gabriel05mss <159582134+gabriel05mss@users.noreply.github.com>
2024-12-24 14:45:01 -0300

    Update ECLAT.R

--- Regras de associação/ECLAT.R
+++ Regras de associação/ECLAT.R
@@ -1,1 +1,22 @@
+#install.packages("arules")
+library(arules)
 
+####leitura da base 
+caminho <- paste0(getwd(),"/dados/GroceryStoreDataSet.csv")
+base <- read.transactions(file = caminho, header = F, sep = ",", rm.duplicates = T)
+####
+
+#### gradico da base de dados outra forma de visualizar
+image(base)
+####
+
+####itemset
+itemset <- eclat(base, parameter = list(support = 0.1))
+inspect(itemset)
+
+itemset <- eclat(base, parameter = list(support = 0.2,maxlen = 2))
+inspect(itemset)
+
+itemset <- eclat(base, parameter = list(support = 0.1,minlen = 2))
+inspect(itemset)
+####
