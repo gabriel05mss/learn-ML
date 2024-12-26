e0e3e1c894bd6822b2c688507d62f263ebe8aa2f
gabriel05mss <159582134+gabriel05mss@users.noreply.github.com>
2024-12-26 18:01:11 -0300

    Update FuzzyCmean.R

--- clusterização/FuzzyCmean.R
+++ clusterização/FuzzyCmean.R
@@ -1,1 +1,21 @@
+library(readr)
+library(e1071)
+vinho = read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data")
+
+colnames(vinho)=c('Tipo','Alcohol','Malic_acid','Ash','Alcalinity_of_ash','Magnesium','Total_phenols', 
+                  'Flavanoids','Nonflavanoid_phenols','Proantocianinas','Proanthocyanins',
+                  'Hue','OD280/OD315','Proline')
+
+vinho1 = vinho[,-1]
+
+vinhoNorm = as.data.frame(scale(vinho1))
+
+vinho_cm = cmeans(vinhoNorm,3,dist = 'euclidean',method = 'cmeans')
+
+vinho_cm$size
+vinho_cm$centers
+previsao <- vinho_cm$cluster
+vinho_cm$membership
+
+clusplot(vinhoNorm,previsao, color = T, lines = F, labels = 5)
 
